15d88770c23e9aa47be7d6ab12a5fc8154d51532
Vansh <vanshpathare900@gmail.com>
2025-12-20 09:19:28 +0530

    Initial project setup

---
+++ Frontend/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
