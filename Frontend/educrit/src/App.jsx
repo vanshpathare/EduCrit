4231211fdd671ee0864d1ced4d833c532bf201e7
Vansh <vanshpathare900@gmail.com>
2026-01-17 18:33:06 +0530

    Frontend structure

--- Frontend/educrit/src/App.jsx
+++ Frontend/educrit/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind is working 🚀
+        Tailwind And EduCrit is working 🚀
       </h1>
     </div>
   );
