3b7b13f912d4be23fe24b05f230abeb1b5815212
Vansh <vanshpathare900@gmail.com>
2026-02-20 20:55:30 +0530

    force rebuild with new env

--- Frontend/educrit/vite.config.js
+++ Frontend/educrit/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
+
+//force build to use env vars from .env file (instead of system env vars) - for development only
